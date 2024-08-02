e2ef14358008f9fce52c71728db208b430c71083
Galina Lukina <lukina.galina11@gmail.com>
2024-08-02 19:09:23 +0400

    add simple_search

---
+++ src/services.py
@@ -0,0 +1,23 @@
+import re
+from src.utils import read_excel_file
+import json
+
+
+def simple_search(transactions_info, search_word):
+    """Возвращает список транзакций с заданным описанием"""
+    found_transactions = []
+    # print(transactions_info.to_dict(orient="records"))
+    for transaction in transactions_info.to_dict(orient="records"):
+        if re.search(search_word, transaction.get("Описание"), flags=re.IGNORECASE):
+            found_transactions.append(transaction)
+
+        if re.search(search_word, str(transaction.get("Категория")), flags=re.IGNORECASE):
+            found_transactions.append(transaction)
+
+    return found_transactions
+
+
+if __name__ == '__main__':
+    data = simple_search(read_excel_file("..\\data\\operations.xlsx"), "перевод")
+
+    print(json.dumps(data, indent=4, ensure_ascii=False))
